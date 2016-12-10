06245061db80f0be3104774425c13e8f9b629dc3
kezuncehng <1666707506@qq.com>
2016-12-10 11:04:26 +0800

    thought i optimized my Slider
    cleaner and eaiser to use :  )

--- slider.js
+++ slider.js
@@ -2,58 +2,33 @@
 // slider.next();//下一页
 // slider.prev();//上一页
 // slider.to(2);//翻到第二页
-let oTimer=null;
-window.onload=function(){
+// var oTimer = null;
+window.onload = function () {
 	var slider=new Slider('#slider',{data:[{img:'imgs/round1.jpg',title:'1'},{img:'imgs/round2.jpg',title:'2'},{img:'imgs/round3.jpg',title:'3'},{img:'imgs/round4.jpg',title:'4'},{img:'imgs/round5.jpg',title:'5'}]});
-	// let oTimer=null;
-	let imgs=document.querySelector('#slider').getElementsByTagName('img');
-	oTimer=setInterval(function(){
-		slider.next();
-	},3000);
 	document.addEventListener('click',function(event){
 		let target=event.target.id;
 		switch(target){
 			case "next":
-				clearInterval(oTimer);
 				slider.next();
-				oTimer=setInterval(function(){
-					slider.next();
-				},3000);
 				break;
 			case "prev":
-				clearInterval(oTimer);
-				slider.prev();
-				oTimer=setInterval(function(){
-					slider.next();
-				},3000);				
+				slider.prev();			
 				break;
 			case "to":
-				clearInterval(oTimer);
 				let page=document.getElementById('text').value.trim();
-				// console.log(iText);
-				if(page>0&&page<=imgs.length&&page%1==0){
 					slider.to(page);
-				}
-				oTimer=setInterval(function(){
-					slider.next();
-				},3000);
 				break;
 		}
 	});
 
 }
 function Slider(container,iData){
-	//var o=new Object();
-	var box=document.querySelector(container);
-	var imgArr=iData.data;//获取图片数组
-	var arrLen=imgArr.length;
-	var imgs=setBox(container,iData);//初始化完成，box里有ul，ul有li，li有img
-	// next:function(){
-	// 	return picMove(true,container);
-	// };
-	// prev:function(){
-	// 	return picMove(false,container);
-	// }
+	const o={};
+	const box=document.querySelector(container);
+	const imgArr=iData.data;//获取图片数组
+	const arrLen=imgArr.length;
+	const imgs=setBox(container,iData);//初始化完成，box里有ul，ul有li，li有img
+	let oTimer=null;
 	for(let i=0;i<imgs.length;i++){
 		imgs[i].addEventListener('mouseover',function(){
 			clearInterval(oTimer);
@@ -65,23 +40,33 @@ function Slider(container,iData){
 			},3000);
 		})
 	}
-	
-	// o.to(page)=function(page){
-	// 	console.log('hi');
-	// };
-	//return o;
-}
-Slider.prototype={
-	next:function(){
+	o.next=function(){
+		clearInterval(oTimer);
+		oTimer=setInterval(function(){
+			picMove(true,'#slider');
+		},3000);
 		return picMove(true,'#slider');
 	},
-	prev:function(){
+	o.prev=function(){
+		clearInterval(oTimer);
+		oTimer=setInterval(function(){
+			picMove(true,'#slider');
+		},3000);
 		return picMove(false,'#slider');
 	},
-	to:function(page){
+	o.to=function(page){
+		clearInterval(oTimer);
+		oTimer=setInterval(function(){
+			picMove(true,'#slider');
+		},3000);
 		return moveTo(page);
-	}
-} 
+	},
+	oTimer=setInterval(function(){
+		o.next();
+	},3000);
+	return o;
+}
+ 
 //初始化box
 //宽度设置为200px，内部插入一个ul，ul里有img个数的li
 //设置li浮动，设置ul和li宽度，li宽度跟box一致
@@ -110,13 +95,13 @@ function setBox(container,iData){
 	ul.style.width=oWidth*arrLen+'px';
 	box.appendChild(ul);//这里完成了box插入一个ul列表,ul列表里有data里img数量的li，每个li里有一个img
 	let aImg=ul.getElementsByTagName('img');
-	return aImg;
+	return aImg;//取出img方便后面用.....
 }
 
 // 图片滚动函数，接受一个参数，true则正序滚动，false则回滚
 function picMove(bool,container){
 	// //滚动效果。设置一个定时器，所有的li向左滚动，滚动距离为图片宽度
-	// //第一次滚动结束后，原本的第一张图片在第二次滚动开始时添加到li队尾
+	// //第一次滚动结束后，原本的第一张图片在第三次滚动开始时添加到li队尾
 	let box=document.querySelector(container);
 	let imgs=box.getElementsByTagName('img');
 	let timer=null;
@@ -159,14 +144,16 @@ function picMove(bool,container){
 function moveTo(num){
 	let imgs=document.querySelector('#slider').getElementsByTagName('img');
 	// let oLeft=(imgs[num].offsetLeft>=0)?imgs[num].offsetLeft:-imgs[num].offsetLeft;
-	let counter=imgs[num-1].offsetLeft/imgs[num-1].offsetWidth;
-	if(counter>0){
-		for(let i=0;i<counter;i++){
-			picMove(true,'#slider');
-		}
-	}else if(counter<0){
-		for(let i=0;counter++;counter<0){
-			picMove(false,'#slider');
+	if(num>0&&num<=imgs.length&&num%1==0){
+		let counter=imgs[num-1].offsetLeft/imgs[num-1].offsetWidth;
+		if(counter>0){
+			for(let i=0;i<counter;i++){
+				picMove(true,'#slider');
+			}
+		}else if(counter<0){
+			for(let i=0;counter++;counter<0){
+				picMove(false,'#slider');
+			}
 		}
 	}
 }
